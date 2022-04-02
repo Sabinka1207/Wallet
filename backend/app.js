295957d1808a6ec174285bd77b33df25e56845cc
eche-unicorn <d.anasteysha@gmail.com>
2022-04-02 21:51:44 +0300

    swagger start

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,32 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const routes = require("./routes/api");
+const routes = require('./routes/api');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", routes.users);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/users', routes.users);
 // app.use("/api/transactions", routes.transactions);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 
