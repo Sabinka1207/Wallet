0faff251404c1623ec78fd89126f871e35ea3459
eche-unicorn <d.anasteysha@gmail.com>
2022-04-03 16:05:41 +0300

    interim

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../../swagger.json');
+const swaggerDocument = require('./swagger.json');
 
 require('dotenv').config();
 
@@ -16,7 +16,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/users', routes.users);
 // app.use("/api/transactions", routes.transactions);
