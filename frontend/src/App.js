ff4558ca48e347d80148d978cb54637ece72341b
Ilja <tadden12@gmail.com>
2022-04-04 14:24:02 +0300

    add lazy & routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,47 @@
-// Import views, routs here
+// import { lazy } from "react";
+// import { Routes, Route } from "react-router-dom";
+// import PrivateRoute from "PrivateRoute";
+// import PublicRoute from "PublicRoute";
 
 import { TestMainView } from "./views";
+
+// const RegisterPage = lazy(() => import("./views/RegisterPage"));
+// const LoginPage = lazy(() => import("./views/LoginPage"));
+// const DashboardPage = lazy(() => import("./views/DashboardPage"));
 
 function App() {
   return (
     <div>
-      <TestMainView></TestMainView>
+      <TestMainView>
+        {/* <Routes> */}
+        {/* <Route
+          path=""
+          element={
+            <PublicRoute restricted redirectTo="">
+              <RegisterPage />
+            </PublicRoute>
+          }
+        ></Route> */}
+
+        {/* <Route
+          path=""
+          element={
+            <PublicRoute restricted redirectTo="">
+              <LoginPage />
+            </PublicRoute>
+          }
+        ></Route> */}
+
+        {/* <Route
+          path=""
+          element={
+            <PrivateRoute redirectTo="">
+              <DashboardPage />
+            </PrivateRoute>
+          }
+        ></Route> */}
+        {/* </Routes> */}
+      </TestMainView>
     </div>
   );
 }
