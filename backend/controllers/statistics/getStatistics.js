4eb2f40feb28c1b9c47ad010010607f54f19580f
Iryna <irasokolova25@ukr.net>
2022-04-05 13:04:33 +0300

    create statistics by month and year

--- backend/controllers/statistics/getStatistics.js
+++ backend/controllers/statistics/getStatistics.js
@@ -5,7 +5,16 @@ const { Transaction } = require('../../models/transaction');
 
 const getStatistics = async (req, res, next) => {
     try {
-      const result = await Transaction.find({});
+      const {date} = req.params
+      console.log(date);
+      const result = await Transaction.aggregate([{ 
+        $project: { 
+          doc: "$$ROOT", 
+          year: { $year: "$date" }, 
+          month: { $month: "$date" }
+        } }, {
+           $match : { "month" : 02, "year": 2022 } 
+          } ] );
       res.json(result)
     } catch (error) {
       next(error)
