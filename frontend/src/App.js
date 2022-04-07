654fa297bb8103ab20c8d589ce236a2ff30466c4
Iryna <irasokolova25@ukr.net>
2022-04-07 22:36:40 +0300

    uncomment routs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader/Loader";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
-import DashboardPage from "./views/DashboardPage";
-// const RegisterPage = lazy(() => import("./views/RegisterPage"));
-// const LoginPage = lazy(() => import("./views/LoginPage"));
-// const DashboardPage = lazy(() => import("./views/DashboardPage"));
+// import DashboardPage from "./views/DashboardPage";
+const RegisterPage = lazy(() => import("./views/RegisterPage"));
+const LoginPage = lazy(() => import("./views/LoginPage"));
+const DashboardPage = lazy(() => import("./views/DashboardPage"));
 
 function App() {
   return (
     <div>
       <DashboardPage/>
-      {/* <Header></Header>
+      <Header></Header>
       <Navigation></Navigation>
       <Suspense fallback={<Loader />}>
         <Routes>
@@ -20,7 +20,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
-      </Suspense> */}
+      </Suspense>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,8 @@ import App from "./App";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     {/* <BrowserRouter> */}
+     <BrowserRouter>
       <App />
-     {/* </BrowserRouter> */}
+     </BrowserRouter>
  </React.StrictMode>
 );
