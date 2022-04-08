dc963d7c29c471275a02e65b8c4e0dc119b9ae17
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2022-04-09 02:14:14 +0300

    add forms logics

--- frontend/src/service/transactions/statisticsApi.js
+++ frontend/src/service/transactions/statisticsApi.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = `http://localhost:3001/api/transactions/`;
+/* axios.defaults.baseURL = `http://localhost:3001/api/transactions/`; */
 
-async function fetchStatistics(){
-    const {data} = await axios.get('statistics');
-    return data;
+async function fetchStatistics() {
+  const { data } = await axios.get("statistics");
+  return data;
 }
 export default fetchStatistics;
