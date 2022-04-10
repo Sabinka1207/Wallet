6f85a34248d713d70da9074e406b09deb1a85316
Olha Dolzhenko <olyasorokolat@gmail.com>
2022-04-09 21:17:18 -0400

    updated styles to login and reg pages

--- frontend/src/views/LoginPage/LoginPage.js
+++ frontend/src/views/LoginPage/LoginPage.js
@@ -8,14 +8,6 @@ import imgDesktop1x from "../../img/desktop/login-desktop@1x.png";
 import imgDesktop2x from "../../img/desktop/login-desktop@2x.png";
 import imgDesktop3x from "../../img/desktop/login-desktop@3x.png";
 import imgDesktop4x from "../../img/desktop/login-desktop@4x.png";
-import ellipseViolet1 from "../../img/Ellipse-1@1x.png";
-import ellipseViolet2 from "../../img/Ellipse-1@2x.png";
-import ellipseViolet3 from "../../img/Ellipse-1@3x.png";
-import ellipseViolet4 from "../../img/Ellipse-1@4x.png"
-import ellipseOrange1 from "../../img/Ellipse-2@1x.png";
-import ellipseOrange2 from "../../img/Ellipse-2@2x.png";
-import ellipseOrange3 from "../../img/Ellipse-2@3x.png";
-import ellipseOrange4 from "../../img/Ellipse-2@4x.png";
 import "../../css/main.min.css";
 
 export default function LoginPage() {
@@ -27,7 +19,7 @@ export default function LoginPage() {
             media="(min-width: 1280px)" />
           <source srcset={`${imgTablet1x} 1x, ${imgTablet2x} 2x, ${imgTablet3x} 3x, ${imgTablet4x} 4x`}
             media="(min-width: 768px)" />
-          <img src={imgTablet1x} alt="" />
+          <img className="loginPageImg" src={imgTablet1x} alt="" />
         </picture>
         
         <p className="loginPageText">Finance App</p>
@@ -35,16 +27,6 @@ export default function LoginPage() {
       <div className="loginPageFormContainer">
         <LoginForm />
       </div>
-        <picture>
-          <source srcset={`${ellipseViolet1} 1x, ${ellipseViolet2} 2x, ${ellipseViolet3} 3x, ${ellipseViolet4} 4x`}
-            />
-          <img className="loginPageEllipseViolet" src={ellipseViolet1} alt="" />
-      </picture>
-      <picture>
-          <source srcset={`${ellipseOrange1} 1x, ${ellipseOrange2} 2x, ${ellipseOrange3} 3x, ${ellipseOrange4} 4x`}
-            />
-          <img className="loginPageEllipseOrange" src={ellipseOrange1} alt="" />
-      </picture>
     </div>
   );
 }
