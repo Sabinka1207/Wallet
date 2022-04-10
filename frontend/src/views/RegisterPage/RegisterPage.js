cbb2ba8d82e4b0396df2c43cbfc8533b410d8155
Olha Dolzhenko <olyasorokolat@gmail.com>
2022-04-10 15:03:25 -0400

    updated styles for login and register pages

--- frontend/src/views/RegisterPage/RegisterPage.js
+++ frontend/src/views/RegisterPage/RegisterPage.js
@@ -15,16 +15,16 @@ export default function LoginPage() {
     <div className="loginPageContainer">
       <div className="loginPageIMGContainer">
         <picture>
-          <source srcset={`${imgDesktop1x} 1x, ${imgDesktop2x} 2x, ${imgDesktop3x} 3x, ${imgDesktop4x} 4x`}
+          <source srcSet={`${imgDesktop1x} 1x, ${imgDesktop2x} 2x, ${imgDesktop3x} 3x, ${imgDesktop4x} 4x`}
             media="(min-width: 1280px)" />
-          <source srcset={`${imgTablet1x} 1x, ${imgTablet2x} 2x, ${imgTablet3x} 3x, ${imgTablet4x} 4x`}
+          <source srcSet={`${imgTablet1x} 1x, ${imgTablet2x} 2x, ${imgTablet3x} 3x, ${imgTablet4x} 4x`}
             media="(min-width: 768px)" />
           <img src={imgTablet1x} alt="" />
         </picture>
         
         <p className="loginPageText">Finance App</p>
       </div>
-      <div className="loginPageFormContainer">
+      <div className="RegisterPageFormContainer">
         <RegistrationForm />
       </div>
     </div>
